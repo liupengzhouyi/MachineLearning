12bf878c755cc189429e994f634e24ee4813013e
liupeng <liupeng.0@outlook.com>
2020-02-10 16:00:32 +0800

    PyTorch regression V 2.0.4

---
+++ 2020-02-10/06Test.py
@@ -0,0 +1,57 @@
+import torch
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
+y = x.pow(2) + 0.2 * torch.rand(x.size())
+
+x = Variable(x)
+y = Variable(y)
+
+# plt.scatter(x.data.numpy(), y.data.numpy())
+# plt.show()
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)
+        self.predict = torch.nn.Linear(n_hidden, n_output)
+
+    def forward(self, x):
+        x = torch.relu(self.hidden(x))
+        x = self.predict(x)
+        return x
+
+net = Net(1, 10, 1)
+print(net)
+
+plt.ion()
+plt.show()
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
+
+loss_func = torch.nn.MSELoss()
+
+for t in range(100):
+    prediction = net(x)
+    # 计算误差
+    loss = loss_func(prediction, y)
+    # 归0
+    optimizer.zero_grad()
+    # 反向传递
+    loss.backward()
+
+    optimizer.step()
+
+    # 接着上面来
+    if t % 5 == 0:
+        # plot and show learning process
+        plt.cla()
+        plt.scatter(x.data.numpy(), y.data.numpy())
+        plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
+        # plt.text(0.5, 0, 'Loss=%.4f' % loss.data[0], fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
